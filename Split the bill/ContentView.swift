5e9bf95b29f48752d96b432bd437e5e964d89ce9
mk812y <mk812y@gmail.com>
2022-08-22 13:46:48 +0300

    Initial Commit

---
+++ Split the bill/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Split the bill
+//
+//  Created by Михаил Куприянов on 22.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Split the bill/Split_the_billApp.swift
@@ -0,0 +1,17 @@
+//
+//  Split_the_billApp.swift
+//  Split the bill
+//
+//  Created by Михаил Куприянов on 22.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Split_the_billApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
